1693a83681f48df1d04791fe97e310e53693aa0b
Illuminater1 <romashcn@gmail.com>
2025-06-08 20:57:28 +0300

    +

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,3 +1,7 @@
+import locale
+from datetime import datetime, timedelta
+
+
 """
 Домашнее задание №2
 
@@ -13,7 +17,14 @@ def print_days():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    locale.setlocale(locale.LC_TIME, "ru_RU")
+
+    dt_now = datetime.now().date()
+    yesterday = dt_now - timedelta(hours=24)
+    one_month_later = dt_now - timedelta(days=30)
+    print(f"Сегодняшняя дата - {dt_now.strftime('%d %B %Y')} год")
+    print(f"Вчерашняя дата - {yesterday.strftime('%d %B %Y')} год")
+    print(f"Дата месяц назад - {one_month_later.strftime('%d %B %Y')} год")
 
 
 def str_2_datetime(date_string):
@@ -21,7 +32,7 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    return datetime.strptime(date_string, "%d/%m/%y %H:%M:%S.%f")
 
 if __name__ == "__main__":
     print_days()
